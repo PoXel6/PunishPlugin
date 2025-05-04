fc3b01e0709332d1aba123bb501cc5485fc6aec0
PoXel6 <iampoxel6@gmail.com>
2025-05-04 13:36:56 +0330

    feat(menu): add PunishMenu#createMenuItem() helper method to add GuiItems to PunishMenu.

--- src/main/java/me/poxel/punishplugin/menu/BanMenu.java
+++ src/main/java/me/poxel/punishplugin/menu/BanMenu.java
@@ -7,7 +7,24 @@ import net.kyori.adventure.text.Component;
 
 public class BanMenu extends BaseMenu {
 
+	//		for (List<String> list : Configuration.getPunishments()) {
+	//		logPunishmentDetails(list);
+	//		final var lore = (Component) List.of(
+	//				Component.text("Reason: " + list.get(2)),
+	//				Component.text("Duration: " + list.get(3)));
+	//		getInventory().addItem(ItemBuilder.from(Material.getMaterial(list.get(0)))
+	//		                                  .name(Component.text(list.get(1)))
+	//		                                  .lore(lore)
+	//		                                  .asGuiItem());
+	//	}
 
+	//	private void logPunishmentDetails(List<String> list) {
+	//		final var logger = PunishPlugin.getInstance().getSLF4JLogger();
+	//		logger.debug("Display Item: {}", list.get(0));
+	//		logger.debug("Name: {}", list.get(1));
+	//		logger.debug("Reason: {}", list.get(2));
+	//		logger.debug("Duration: {}", list.get(3));
+	//	}
 	@Override
 	public int getRows() {
 		return 6;
