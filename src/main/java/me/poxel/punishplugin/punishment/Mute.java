e9e46b3a95831115b13ace741dbdcf900acf9544
PoXel6 <iampoxel6@gmail.com>
2025-05-04 07:15:13 +0330

    feat(punishment): add Mute#muteList to Mute.java.

--- src/main/java/me/poxel/punishplugin/punishment/Mute.java
+++ src/main/java/me/poxel/punishplugin/punishment/Mute.java
@@ -2,9 +2,12 @@ package me.poxel.punishplugin.punishment;
 
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 
-public abstract class Mute implements Punishable{
+public abstract class Mute implements Punishable {
+
+
+	public static final List<Player> muteList = new LinkedList<>();
 }
