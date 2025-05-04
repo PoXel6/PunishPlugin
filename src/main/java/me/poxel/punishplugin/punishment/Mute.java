c491a1fb06068e145165d790b8f39dfc302c274f
PoXel6 <iampoxel6@gmail.com>
2025-05-04 07:14:00 +0330

    feat(punishment): made Mute.java.

---
+++ src/main/java/me/poxel/punishplugin/punishment/Mute.java
@@ -0,0 +1,10 @@
+package me.poxel.punishplugin.punishment;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public abstract class Mute implements Punishable{
+}
