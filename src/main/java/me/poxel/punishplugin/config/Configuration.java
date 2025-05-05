1cf0031c4582be150ff9bbe65437e8b859a644d3
PoXel6 <iampoxel6@gmail.com>
2025-05-05 10:32:45 +0330

    feat(config): Punishments now are loaded based on the punishment type section.

--- src/main/java/me/poxel/punishplugin/config/Configuration.java
+++ src/main/java/me/poxel/punishplugin/config/Configuration.java
@@ -2,6 +2,7 @@ package me.poxel.punishplugin.config;
 
 import lombok.Getter;
 import net.kyori.adventure.text.Component;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.ArrayList;
@@ -16,12 +17,14 @@ public final class Configuration {
 	@Getter private static Component punishMenuName;
 	@Getter private static Component BanMenuName;
 	@Getter private static Component MuteMenuName;
+	@Getter private static ConfigurationSection mutePunishments;
+	private static ConfigurationSection banPunishments;
 
 	public static void loadConfig(FileConfiguration config) {
-		punishments = fetchPunishments(config);
 		punishMenuName = ConfigManager.deserialize(config.getString("Menus.Punish", "Punish Menu"));
 		BanMenuName = ConfigManager.deserialize(config.getString("Menus.Ban", "Ban Menu"));
 		MuteMenuName = ConfigManager.deserialize(config.getString("Menus.Mute", "Mute Menu"));
+		loadPunishments(config);
 	}
 
 	private static List<List<String>> fetchPunishments(FileConfiguration config) {
@@ -35,4 +38,10 @@ public final class Configuration {
 		                                    : new ArrayList<>()));
 		return punishments;
 	}
+
+	private static void loadPunishments(FileConfiguration config) {
+		mutePunishments = config.getConfigurationSection("Punishments.mute");
+		banPunishments = config.getConfigurationSection("Punishments.ban");
+	}
+
 }
