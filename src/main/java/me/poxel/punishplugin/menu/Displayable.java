94493046170b888bb984807725187613a3d8db8e
PoXel6 <iampoxel6@gmail.com>
2025-05-05 16:50:44 +0330

    feat(menu): add Displayable for classes that hold Punishments.

--- src/main/java/me/poxel/punishplugin/menu/Displayable.java
+++ src/main/java/me/poxel/punishplugin/menu/Displayable.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-public interface DisplayPunishments {
+public interface Displayable {
 
 
 	default List<GuiItem> createPunishItem(List<List<String>> punish) {
---
+++ src/main/java/me/poxel/punishplugin/menu/Loadable.java
@@ -0,0 +1,12 @@
+package me.poxel.punishplugin.menu;
+
+import java.util.List;
+
+
+public interface Loadable {
+
+
+	List<List<String>> loadPunishments();
+
+
+}
