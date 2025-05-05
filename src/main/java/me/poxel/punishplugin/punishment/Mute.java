b7c6b85514a91f9d4e5d3361f5012c7834bb9865
PoXel6 <iampoxel6@gmail.com>
2025-05-05 16:39:24 +0330

    fix(Punishment): fix Mute.java be an abstract class.

--- src/main/java/me/poxel/punishplugin/punishment/Mute.java
+++ src/main/java/me/poxel/punishplugin/punishment/Mute.java
@@ -6,8 +6,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 
-public abstract class Mute implements Punishable {
+public class Mute implements Punishable {
 
 
 	public static final List<Player> muteList = new LinkedList<>();
+
+	@Override
+	public void punish() {
+
+	}
 }
