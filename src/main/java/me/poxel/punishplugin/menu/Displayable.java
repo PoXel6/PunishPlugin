0684339e4c1c0de291d2185b0705dad79c436156
PoXel6 <iampoxel6@gmail.com>
2025-05-06 08:34:10 +0330

    refactor(menu): made displayable more robust and verbose.

--- src/main/java/me/poxel/punishplugin/menu/Displayable.java
+++ src/main/java/me/poxel/punishplugin/menu/Displayable.java
@@ -2,10 +2,13 @@ package me.poxel.punishplugin.menu;
 
 import dev.triumphteam.gui.builder.item.ItemBuilder;
 import dev.triumphteam.gui.guis.GuiItem;
-import me.poxel.punishplugin.punishment.PunishFields;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,19 +16,18 @@ import java.util.Optional;
 public interface Displayable {
 
 
-	default List<GuiItem> createPunishItem(List<List<String>> punish) {
-		return punish.stream().map(ban -> {
-			final var item = Material.getMaterial(ban.get(PunishFields.ITEM.ordinal()));
-			final var name = Component.text(ban.get(PunishFields.NAME.ordinal()));
-			final var reason = Component.text("Reason: " + ban.get(PunishFields.REASON.ordinal()));
-			final var duration = Component.text("Duration: " + ban.get(PunishFields.DURATION.ordinal()));
-
-			return ItemBuilder.from(Optional.ofNullable(item).orElse(Material.STONE))
-			                  .name(Optional.of(name).orElse(Component.text("Name is null")))
-			                  .lore(Optional.of(reason).orElse(Component.text("Reason is null")))
-			                  .lore(Optional.of(duration).orElse(Component.text("Duration is null")))
-			                  .asGuiItem();
-		}).toList();
+	default List<GuiItem> punishmentsAsItem(ConfigurationSection section) {
+		final var listItem = new ArrayList<GuiItem>();
+		for (final String key : section.getKeys(true)) {
+			final var list = section.getStringList(key);
+			final var item = ItemBuilder
+					.from(Optional.ofNullable(Material.getMaterial(list.get(0))).orElse(Material.STONE))
+					.name(Component.text(list.get(1)).style(Style.style(TextDecoration.BOLD)))
+					.lore(Component.text("Reason: " + list.get(2)))
+					.lore(Component.text("Duration: " + list.get(3))).asGuiItem();
+			listItem.add(item);
+		}
+		return listItem;
 	}
 
 
