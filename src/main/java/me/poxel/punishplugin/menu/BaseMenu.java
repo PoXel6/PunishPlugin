83e02d8c66fe0887ccc41c7430e8748e249c25bd
PoXel6 <iampoxel6@gmail.com>
2025-05-06 17:12:29 +0330

    chore: marge api package.

---
+++ src/main/java/me/poxel/punishplugin/menu/BaseMenu.java
@@ -0,0 +1,33 @@
+package me.poxel.punishplugin.menu;
+
+import dev.triumphteam.gui.guis.Gui;
+import org.bukkit.entity.Player;
+
+
+public abstract class BaseMenu implements Menu {
+
+
+	private Gui inventory;
+
+	@Override
+	public void open(final Player player) {
+		inventory = Gui.gui().title(getTitle()).rows(getRows()).create();
+		setMenuItem();
+		inventory.open(player);
+		setAction();
+	}
+
+	@Override
+	public void setAction() {
+		getInventory().setDefaultClickAction(event -> {
+			if (event.getCurrentItem() == null)
+				return;
+			event.setCancelled(true);
+		});
+	}
+
+	@Override
+	public Gui getInventory() {
+		return inventory;
+	}
+}
