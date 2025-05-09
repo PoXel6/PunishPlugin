1a87c6c59d66cef0639aa60c2026f0903b85af9a
PoXel6 <iampoxel6@gmail.com>
2025-05-09 11:53:37 +0330

    refactor(main): Extract registering command and listener to their own method with a vararg parameter.

--- src/main/java/me/poxel/punishplugin/PunishPlugin.java
+++ src/main/java/me/poxel/punishplugin/PunishPlugin.java
@@ -1,12 +1,17 @@
 package me.poxel.punishplugin;
 
+import co.aikar.commands.BaseCommand;
 import co.aikar.commands.PaperCommandManager;
 import lombok.Getter;
 import me.poxel.punishplugin.command.PunishCommand;
 import me.poxel.punishplugin.config.ConfigManager;
 import me.poxel.punishplugin.config.Configuration;
 import me.poxel.punishplugin.listener.MuteListener;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 
 public final class PunishPlugin extends JavaPlugin {
@@ -20,11 +25,23 @@ public final class PunishPlugin extends JavaPlugin {
 	public void onEnable() {
 		this.getLogger().info("Loading PunishPlugin...");
 		instance = this;
-		loadConfig();
 		manager = new PaperCommandManager(this);
-		manager.registerCommand(new PunishCommand());
 		manager.enableUnstableAPI("help");
-		getServer().getPluginManager().registerEvents(new MuteListener(),this);
+		loadConfig();
+		registerCommands(new PunishCommand());
+		registerListeners(new MuteListener());
+	}
+
+	private void registerListeners(@NotNull Listener... listeners) {
+		this.getLogger().info("Loading Listeners...");
+		Arrays.stream(listeners).forEach(listener -> {
+			getServer().getPluginManager().registerEvents(listener, this);
+		});
+	}
+
+	private void registerCommands(@NotNull BaseCommand... commands) {
+		this.getLogger().info("Loading Commands...");
+		Arrays.stream(commands).forEach(command -> manager.registerCommand(command));
 	}
 
 	private void loadConfig() {
