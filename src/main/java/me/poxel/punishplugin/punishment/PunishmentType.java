70fe23cda9f00a613d52d76259645fc7aed448a0
PoXel6 <iampoxel6@gmail.com>
2025-05-09 17:09:35 +0330

    feat(punishment): PunishmentType enum now holds Punishment's menu and punishment's Material type.

--- src/main/java/me/poxel/punishplugin/punishment/PunishmentType.java
+++ src/main/java/me/poxel/punishplugin/punishment/PunishmentType.java
@@ -2,20 +2,25 @@ package me.poxel.punishplugin.punishment;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import me.poxel.punishplugin.menu.BaseMenu;
+import me.poxel.punishplugin.menu.punishment.BanMenu;
+import me.poxel.punishplugin.menu.punishment.MuteMenu;
+import org.bukkit.Material;
 
 
 @Getter
 @RequiredArgsConstructor
 public enum PunishmentType {
-	BAN("BAN"),
-	MUTE("MUTE"),
-	TEMP_BAN("TEMP_BAN"),
-	TEMP_MUTE("TEMP_MUTE"),
-	SHADOW_MUTE("SHADOW_MUTE"),
-	TEMP_SHADOW_MUTE("TEMP_SHADOW_MUTE"),
-	KICK("KICK"),
-	WARN("WARN"),
-	IP_BAN("IP_BAN"),
-	IP_MUTE("IP_MUTE");
-	private final String mode;
+	BAN(new BanMenu(), Material.WOODEN_AXE),
+	MUTE(new MuteMenu(), Material.NOTE_BLOCK);
+	//	TEMP_BAN(),
+	//	TEMP_MUTE(),
+	//	SHADOW_MUTE(),
+	//	TEMP_SHADOW_MUTE(),
+	//	KICK(),
+	//	WARN(),
+	//	IP_BAN(),
+	//	IP_MUTE();
+	private final BaseMenu menu;
+	private final Material item;
 }
