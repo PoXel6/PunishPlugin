9b7580452d9addfc59e19b2c39cbc6362d2d7503
PoXel6 <iampoxel6@gmail.com>
2025-05-10 20:28:41 +0330

    feat(config): load permissions from config.

--- src/main/java/me/poxel/punishplugin/config/Configuration.java
+++ src/main/java/me/poxel/punishplugin/config/Configuration.java
@@ -14,10 +14,16 @@ public final class Configuration {
 	@Getter private static Component MuteMenuName;
 	@Getter private static ConfigurationSection mutePunishments;
 	@Getter private static ConfigurationSection banPunishments;
+	@Getter private static String globalPermission;
+	@Getter private static String punishPermission;
+	@Getter private static String reloadPermission;
+	@Getter private static String reloadConfigPermission;
+	@Getter private static String reloadGuiPermission;
 
 	public static void loadConfig(FileConfiguration config) {
 		loadMenuNames(config);
 		loadPunishments(config);
+		loadPermissions(config);
 	}
 
 	private static void loadMenuNames(FileConfiguration config) {
@@ -29,6 +35,14 @@ public final class Configuration {
 	private static void loadPunishments(FileConfiguration config) {
 		mutePunishments = config.getConfigurationSection("PUNISHMENT.mute");
 		banPunishments = config.getConfigurationSection("PUNISHMENT.ban");
+	}
+
+	private static void loadPermissions(FileConfiguration config) {
+		globalPermission = config.getString("punish");
+		punishPermission = config.getString("punish.punish");
+		reloadPermission = config.getString("punish.reload");
+		reloadConfigPermission = config.getString("punish.reload.config");
+		reloadGuiPermission = config.getString("punish.reload.gui");
 	}
 
 }
